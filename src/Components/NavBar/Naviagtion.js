1ae55b0e3fc1b3afeecac674e4157ab4a8ee6e5a
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-05-13 00:51:32 +0530

    till logo and location

--- src/Components/NavBar/Naviagtion.js
+++ src/Components/NavBar/Naviagtion.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import "./NavBar.css";
 
 class Navigation extends Component {
   constructor(props) {
@@ -9,7 +9,19 @@ class Navigation extends Component {
 
   render() {
     return (
-      <div>Navigation</div>
+      <div className="navbar_component">
+        Navigation
+        <div className="navbar_logo"></div>
+        <div className="navbar_locator">
+          <div className="navbar_locatorImage"></div>
+          <div className="navbar_location">Banglore</div>
+        </div>
+        <div className="navbar_searchmain">
+            <div></div>
+            <div className="navbar_searchbar" input="text"></div>
+            <div></div>
+        </div>
+      </div>
     );
   }
 }
