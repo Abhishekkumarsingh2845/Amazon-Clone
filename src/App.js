0adc1aa6e7f93a3ade7305045ed9d24a5b19835f
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-05-12 21:34:06 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainPage from './Components/HomePage/MainPage'
+import Navigation from './Components/NavBar/Naviagtion'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigation/>
+      <MainPage/>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/Naviagtion.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Navigation</div>
+    );
+  }
+}
+
+export default Navigation;
